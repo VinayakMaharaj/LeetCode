962f21829cd21d819e9302d33e2d4b0ffcb5bc25
Vinayak <vinayakcpa@gmail.com>
2023-06-23 17:40:46 -0400

    added 2623

---
+++ medium/JS/2623. Memoize.ts
@@ -0,0 +1,17 @@
+type Fn = (...params: any) => any;
+
+var memoize =
+  (fn: (...params: any) => any, cache = {}) =>
+  (...args) =>
+    cache[args.join()] ?? (cache[args.join()] = fn(...args));
+
+/**
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1
+ */
