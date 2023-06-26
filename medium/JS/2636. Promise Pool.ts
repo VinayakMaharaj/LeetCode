0bcd34086e61664dd320bef1716ef4ec9485bb8a
Vinayak <vinayakcpa@gmail.com>
2023-06-26 18:12:00 -0400

    added 2636

---
+++ medium/JS/2636. Promise Pool.ts
@@ -0,0 +1,11 @@
+type F = () => Promise<any>;
+
+async function promisePool(functions: F[], n: number): Promise<any> {
+  const evaluateNext = () => functions[n++]?.().then(evaluateNext);
+  return Promise.all(functions.slice(0, n).map((f) => f().then(evaluateNext)));
+}
+/**
+ * const sleep = (t) => new Promise(res => setTimeout(res, t));
+ * promisePool([() => sleep(500), () => sleep(400)], 1)
+ *   .then(console.log) // After 900ms
+ */
