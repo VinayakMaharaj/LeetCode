cc50fb082610bf908b7f1102df69de11bf608592
Vinayak <vinayakcpa@gmail.com>
2023-08-08 13:56:01 -0400

    added 1173. Immediate Food Delivery I.py

---
+++ easy/pandas/1173. Immediate Food Delivery I.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+def food_delivery(delivery: pd.DataFrame) -> pd.DataFrame:
+    return pd.DataFrame({'immediate_percentage': [
+        round(delivery[delivery['order_date'] ==
+              delivery['customer_pref_delivery_date']].size / delivery.size * 100, 2)
+    ]})
