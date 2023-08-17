42b263e2a6cd849b36836020546f5adb1e624678
Vinayak <vinayakcpa@gmail.com>
2023-08-17 19:47:13 -0400

    added 1407.sql

---
+++ easy/SQL/1407.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+
+SELECT Users.name, IFNULL(SUM(Rides.distance), 0) as travelled_distance FROM Users LEFT JOIN Rides ON Users.id = Rides.user_id GROUP BY Rides.user_id ORDER BY travelled_distance DESC, Users.name ;
