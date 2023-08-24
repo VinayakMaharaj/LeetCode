ed7aab33be2ff929d9f9264b63f38de1fbc85740
Vinayak <vinayakcpa@gmail.com>
2023-08-24 15:27:24 -0400

    added 512. Game Play Analysis II

---
+++ easy/SQL/512. Game Play Analysis II.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+SELECT player_id, device_id
+FROM (SELECT player_id, device_id, event_date,
+    ROW_NUMBER() OVER(PARTITION BY player_id
+                      ORDER BY event_date) as e_row
+     FROM Activity) t
+WHERE t.e_row = 1;
