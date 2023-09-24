023793e3f25171ccd66249e5f3b7f3110ca38bf6
Vinayak <vinayakcpa@gmail.com>
2023-09-24 15:13:17 -0400

    added 799. Champagne Tower.js

---
+++ easy/JS/799. Champagne Tower.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} poured
+ * @param {number} query_row
+ * @param {number} query_glass
+ * @return {number}
+ */
+var champagneTower = function (poured, query_row, query_glass) {
+  const dp = new Array(query_row + 2).fill(0); // One extra row for simplicity
+
+  dp[0] = poured;
+
+  for (let row = 1; row <= query_row; row++) {
+    for (let glass = row; glass >= 0; glass--) {
+      dp[glass + 1] += dp[glass] = Math.max(0, (dp[glass] - 1) / 2);
+    }
+  }
+
+  return Math.min(1, dp[query_glass]);
+};
